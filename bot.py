f78a6f86de9fb13526a1cf63d1ce3d1191dac511
HOOPSER <36602208+HOOPSER@users.noreply.github.com>
2018-02-20 17:46:19 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,1 @@
-{
-            "name": "Python",
-            "type": "python",
-            "request": "launch",
-            "stopOnEntry": false,
-            "pythonPath": "${config:python.pythonPath}",
-            "program": "${file}",
-            "cwd": "${workspaceFolder}",
-            "env": {},
-            "envFile": "${workspaceFolder}/.env",
-            "debugOptions": [
-                "RedirectOutput"
-            ]
-        },
-        {
+
